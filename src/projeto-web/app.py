e486209ecc3b0899ce9615f771d35d0ebb539419
MuriloDeSouza <murilo.silva@sou.inteli.edu.br>
2024-03-08 16:44:31 -0300

    a

--- src/projeto-web/app.py
+++ src/projeto-web/app.py
@@ -1,4 +1,3 @@
-#app.py
 from flask import Flask, render_template, request, redirect, url_for
 from tinydb import TinyDB
 
@@ -7,19 +6,11 @@ app = Flask(__name__)
 db = TinyDB("posts.json")
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
-# @app.route("/sobre", methods=["GET", "POST"])
-# def sobre(nome=None):
-#     if request.method == "POST":
-#         nome = request.form.get("nome")
-#         db.insert({"nome": nome})
-#     posts = db.all()
-#     return render_template("sobre.html", nome=nome, posts=posts)
-
 @app.route("/novo", methods=["GET", "POST"])
-def novo(rota = None):
+def novo():
     if request.method == "POST":
         rota = request.form.get("rota")
         db.insert({"X": rota})
@@ -31,24 +22,23 @@ def novo(rota = None):
 
 @app.route("/pegar_caminho", methods=["GET"])
 def pegar_caminho():
-    return render_template("pegar_caminho.html", pegar_caminho=pegar_caminho)
+    return render_template("pegar_caminho.html")
 
 @app.route("/atualizar", methods=["GET"])
-def index():
+def atualizar():
     return render_template("atualizar.html")
 
 @app.route("/deletar", methods=["DELETE"])
-def index():
+def deletar():
     return render_template("deletar.html")
 
 @app.route("/listas_caminhos", methods=["GET"])
-def index():
+def listas_caminhos():
     return render_template("Listas_caminhos.html")
 
 @app.route("/pegar_caminhos", methods=["GET"])
-def index():
+def pegar_caminhos():
     return render_template("pegar_caminhos.html")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
